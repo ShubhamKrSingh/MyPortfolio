facbc8155104f957e760904b24a576a6b25bb4c3
shubham kumar <43433874+ShubhamKrSingh@users.noreply.github.com>
2023-09-19 07:19:35 +0000

    script sepration

---
+++ script.js
@@ -0,0 +1,7 @@
+src="https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js"
+
+var typed = new Typed('#element', {
+    strings: ['Web Developer', 'Software Devloper','Web Designer','UI Designer'],            
+    typeSpeed: 60,
+    loop: true
+});
